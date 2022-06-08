0c604a99e40727ce42139a4083c087f06a256cd4
osvegn <thomas.prudhomme@epitech.eu>
2022-06-08 15:14:50 +0200

    [+] add setter prototype for sfml event handling

--- src/zappy_gui/Core/SFML/WrapperSFML/Event.hpp
+++ src/zappy_gui/Core/SFML/WrapperSFML/Event.hpp
@@ -10,12 +10,32 @@
 #ifndef EVENT_HPP_
 #define EVENT_HPP_
 
+#include <SFML/Graphics.hpp>
+
 /// \brief The class event handle events from SFML to be use anywhere in the zappy_gui.
 /// It will simplify the use of all event from keyboard and mouse button.
 class Event {
     public:
-        Event() = default;
+        Event() : _keyPressed(false), _buttonPressed(false), _altPressed(false), _ctrlPressed(false), _shiftPressed(false), _key(sf::Keyboard::A), _button(sf::Mouse::Left) {};
+        void keyPressed(sf::Keyboard::Key key = sf::Keyboard::A);
+        void keyReleased();
+        void buttonPressed(sf::Mouse::Button button = sf::Mouse::Left);
+        void buttonReleased();
+        void altPressed();
+        void altReleased();
+        void ctrlPressed();
+        void ctrlReleased();
+        void shiftPressed();
+        void shiftReleased();
         ~Event() = default;
+    private:
+        bool _keyPressed;
+        bool _buttonPressed;
+        bool _altPressed;
+        bool _ctrlPressed;
+        bool _shiftPressed;
+        sf::Keyboard::Key _key;
+        sf::Mouse::Button _button;
 };
 
 #endif /* !EVENT_HPP_ */
