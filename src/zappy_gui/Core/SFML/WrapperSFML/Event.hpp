a43f03b4ff8139dd219ef309c9e7ff45ac8f11ea
osvegn <thomas.prudhomme@epitech.eu>
2022-06-08 15:17:53 +0200

    [+] add logic part in sfml event handling

--- src/zappy_gui/Core/SFML/WrapperSFML/Event.hpp
+++ src/zappy_gui/Core/SFML/WrapperSFML/Event.hpp
@@ -17,16 +17,38 @@
 class Event {
     public:
         Event() : _keyPressed(false), _buttonPressed(false), _altPressed(false), _ctrlPressed(false), _shiftPressed(false), _key(sf::Keyboard::A), _button(sf::Mouse::Left) {};
-        void keyPressed(sf::Keyboard::Key key = sf::Keyboard::A);
-        void keyReleased();
-        void buttonPressed(sf::Mouse::Button button = sf::Mouse::Left);
-        void buttonReleased();
-        void altPressed();
-        void altReleased();
-        void ctrlPressed();
-        void ctrlReleased();
-        void shiftPressed();
-        void shiftReleased();
+        inline void keyPressed(sf::Keyboard::Key key = sf::Keyboard::A) {
+            _key = key;
+            _keyPressed = true;
+        };
+        inline void keyReleased() {
+            _keyPressed = false;
+        };
+        inline void buttonPressed(sf::Mouse::Button button = sf::Mouse::Left) {
+            _button = button;
+            _buttonPressed = true;
+        };
+        inline void buttonReleased() {
+            _buttonPressed = false;
+        };
+        inline void altPressed() {
+            _altPressed = true;
+        };
+        inline void altReleased() {
+            _altPressed = false;
+        };
+        inline void ctrlPressed() {
+            _ctrlPressed = true;
+        };
+        inline void ctrlReleased() {
+            _ctrlPressed = false;
+        };
+        inline void shiftPressed() {
+            _shiftPressed = true;
+        };
+        inline void shiftReleased() {
+            _shiftPressed = false;
+        };
         ~Event() = default;
     private:
         bool _keyPressed;
