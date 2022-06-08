6113f353e8b5d59d52299ef13f97f46e8a98d853
osvegn <thomas.prudhomme@epitech.eu>
2022-06-08 15:05:35 +0200

    [+] create the Event class

---
+++ src/zappy_gui/Core/SFML/WrapperSFML/Event.hpp
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2022
+** zappy
+** File description:
+** Event
+*/
+
+/// \file src/zappy_gui/Core/SFML/WrapperSFML/Event.hpp
+
+#ifndef EVENT_HPP_
+#define EVENT_HPP_
+
+/// \brief The class event handle events from SFML to be use anywhere in the zappy_gui.
+/// It will simplify the use of all event from keyboard and mouse button.
+class Event {
+    public:
+        Event() = default;
+        ~Event() = default;
+};
+
+#endif /* !EVENT_HPP_ */
