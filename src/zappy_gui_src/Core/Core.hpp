c77ff8bce72579bfd62305a655a4610c0f2db4e6
osvegn <thomas.prudhomme@epitech.eu>
2022-06-09 16:16:21 +0200

    [+] add docuentation in the core.hpp

--- src/zappy_gui_src/Core/Core.hpp
+++ src/zappy_gui_src/Core/Core.hpp
@@ -17,25 +17,55 @@
 /// \brief The core of the zappy_gui. It's the most important part of the project.
 class Core {
     public:
+        /// \brief Exception for the Core.
         class CoreException : std::exception {
             public:
+
+                /// \brief Constructor of the CoreException.
+                /// \param title The title of the exception.
+                /// \param description The description of the exception.
                 CoreException(std::string title = "", std::string description = "");
+
+                /// \brief Destructor of the CoreException.
+                /// \return The exception.
                 const char *what() const noexcept override;
             private:
+
+                /// \brief The title of the exception.
                 std::string _title;
+
+                /// \brief The description of the exception.
                 std::string _description;
+
+                /// \brief The message of the exception.
                 std::string _what;
         };
+
         /// \brief Constructor of the Core.
         Core() = default;
+
         /// \brief Destructor of the Core.
         ~Core() = default;
+
+        /// \brief Run the core. It's the main loop of the program.
         void run();
+
+        /// \brief Setup the core with the given parameters.
         void setup(int ac, char **av);
     private:
+
+        /// \brief Get the parameters of the program and set the core with them.
+        /// \param ac The number of parameters.
+        /// \param av The parameters.
         void _getArgs(int ac, char **av);
+
+        /// \brief The port of the server.
         std::string _port;
+
+        /// \brief The ip of the server.
         std::string _machine;
+
+        /// \brief An instance of the sfml graphical part.
         SFML _sfml;
 };
 
