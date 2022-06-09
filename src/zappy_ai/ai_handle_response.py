ebb4a85d679af529c07ffc95741de5a721b543d5
Nathan <nathan.rousseau@epitech.eu>
2022-06-09 10:35:51 +0200

    [+] - Add Getter for Inventory #62

--- src/zappy_ai/ai_handle_response.py
+++ src/zappy_ai/ai_handle_response.py
@@ -6,7 +6,7 @@ class Inventory:
         self.__linemate : int = 0
         self.__deraumere : int = 0
         self.__sibur : int = 0
-        self.__mediane : int = 0
+        self.__mendiane : int = 0
         self.__phiras : int = 0
         self.__thysmane : int = 0
         if not self.__fillInventory(inventoryResponse):
@@ -14,3 +14,24 @@ class Inventory:
 
     def __fillInventory(inventoryStr : str) -> bool:
         return True
+
+    def GetFood(self) -> int:
+        return self.__food
+
+    def GetLinemate(self) -> int:
+        return self.__linemate
+
+    def GetDeraumere(self) -> int:
+        return self.__deraumere
+
+    def GetSibur(self) -> int:
+        return self.__sibur
+
+    def GetMendiane(self) -> int:
+        return self.__mendiane
+
+    def GetPhiras(self) -> int:
+        return self.__phiras
+
+    def GetThysmane(self) -> int:
+        return self.__thysmane
