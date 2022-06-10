650e1a3e0b77b381a8c629e83b223f9562f985b0
osvegn <thomas.prudhomme@epitech.eu>
2022-06-10 07:51:06 +0200

    [*] remove Tile.cpp/.hpp files, move usage in a function

--- src/zappy_gui_src/main.cpp
+++ src/zappy_gui_src/main.cpp
@@ -11,28 +11,32 @@
 #include <iostream>
 #include <string>
 
+static int usage(int ac, char **av)
+{
+    if (ac == 2 && (std::string(av[1]) == "-h" || std::string(av[1]) == "--help")) {
+        std::cerr << "USAGE: ./zappy_ai -p port -h machine\n\tport\t is the port number\n\tmachine\t is the name of the machine; localhost by default" << std::endl;
+        return -1;
+    }
+    return 0;
+}
+
 int main(int ac, char **av)
 {
     Core core;
 
-    if (ac == 2 && (std::string(av[1]) == "-h" || std::string(av[1]) == "--help")) {
-        std::cerr << "USAGE: ./zappy_ai -p port -h machine" <<
-        std::endl << "\tport\t is the port number" <<std::endl <<
-        "\tmachine\t is the name of the machine; localhost by default" <<
-        std::endl;
+    if (usage(ac, av) < 0)
         return 84;
-    }
     try {
         core.setup(ac, av);
     } catch (Core::CoreException &e) {
         std::cerr << e.what() << std::endl;
-        return (84);
+        return 84;
     }
     try {
         core.run();
     } catch (Core::CoreException &e) {
         std::cerr << e.what() << std::endl;
-        return (84);
+        return 84;
     }
     return 0;
 }
