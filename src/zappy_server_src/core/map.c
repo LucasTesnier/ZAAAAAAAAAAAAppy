1464be66f528232e0c8a0cbb6d1ce7655b408c97
Cavonstavant <constant.vigneron@protonmail.com>
2022-06-13 17:21:25 +0200

    [+] Create tile entity when init a map

--- src/zappy_server_src/core/map.c
+++ src/zappy_server_src/core/map.c
@@ -7,7 +7,18 @@
 
 #include "map.h"
 #include "entity/entity.h"
+#include "entity/tile.h"
+#include "entity/entity_types.h"
 #include <stdlib.h>
+
+entity_t* get_tile(map_t *map, int x, int y)
+{
+    if (!map)
+        return NULL;
+    if (x < 0 || x >= map->width || y < 0 || y >= map->height)
+        return NULL;
+    return map->tiles[y * map->width + x];
+}
 
 map_t *create_new_map(int width, int height)
 {
@@ -18,14 +29,12 @@ map_t *create_new_map(int width, int height)
     map->tiles = malloc(sizeof(entity_t *) * height * width);
     if (!map->tiles)
         return NULL;
+    for (int i = 0; i < height; i++){
+        for (int j = 0; j < width; j++){
+            map->tiles[i * width + j] = create_entity(ENTITY_TILE_TYPE,
+                (position_t){j, i});
+            map->tiles[i * width + j]->data = create_new_tile();
+        }
+    }
     return map;
-}
-
-entity_t* get_tile(map_t *map, int x, int y)
-{
-    if (!map)
-        return NULL;
-    if (x < 0 || x >= map->width || y < 0 || y >= map->height)
-        return NULL;
-    return map->tiles[y * map->width + x];
 }
