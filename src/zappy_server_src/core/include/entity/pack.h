bff957e1ac7faaf3e237fa78c83c4e6092b29442
Guillaume <guillaume.terriere@epitech.eu>
2022-06-14 16:30:36 +0200

    [+] separate pack functions in files

--- src/zappy_server_src/core/include/entity/pack.h
+++ src/zappy_server_src/core/include/entity/pack.h
@@ -5,7 +5,7 @@
 ** pack
 */
 
-/// \file src/zappy_gui_src/Core/Network/pack.h
+/// \file src/zappy_server_src/core/include/entity/pack.h
 
 #ifndef PACK_H_
     #define PACK_H_
@@ -15,7 +15,7 @@
     #include "container.h"
     #include "player.h"
     #include "tile.h"
-    #include "egg.h"
+    #include "eggs.h"
 
 /// \brief generic way of packing an int
 /// \return a serialized string representing an int
@@ -24,6 +24,26 @@ char *pack_int(int val);
 /// \brief generic way of packing a string
 /// \return a serialized string representing a string
 char *pack_str(char *val);
+
+/// \brief get the len of an int
+/// \return the len of the string representing the value
+int get_int_len(int val);
+
+/// \brief get the len of a container
+/// \return the len of the string representing a container
+int get_len_container(container_t *cont);
+
+/// \brief get the len of a player
+/// \return the len of the string representing a player
+int get_len_player(entity_t *ent, player_t *p);
+
+/// \brief get the len of a tile
+/// \return the len of the string representing a tile
+int get_len_tile(entity_t *ent, tile_t *t);
+
+/// \brief get the len of a egg
+/// \return the len of the string representing a egg
+int get_len_egg(entity_t *ent, egg_t *t);
 
 /// \brief pack a generic container for an inventory or the content of a tile
 /// \return a serialized string representing the inventory
