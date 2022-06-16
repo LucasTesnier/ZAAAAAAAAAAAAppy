7a5fec9844b085f02e3200346cb14ad38a9882be
Cavonstavant <constant.vigneron@protonmail.com>
2022-06-16 12:35:53 +0200

    [+] Move commands source into dedicated folder

--- src/zappy_server_src/core/commands/auth_command.c
+++ src/zappy_server_src/core/commands/auth_command.c
@@ -10,9 +10,7 @@
 #include "command_hold.h"
 #include "rcodes.h"
 #include "team.h"
-#include "entity/entity.h"
-#include "entity/player.h"
-#include <stdlib.h>
+
 bool command_logout(char *arg, player_list_t *player, server_data_t *serv)
 {
     char *type = NULL;
@@ -50,27 +48,9 @@ bool command_login(char *arg, player_list_t *player, server_data_t *serv)
     return print_retcode(201, type, player->player_peer, true);
 }
 
-static bool command_join_create_player_data(char *arg,
-entity_t **player_entity, server_data_t *serv)
-{
-    player_t *player_data = NULL;
-
-    *player_entity = create_entity(ENTITY_PLAYER_TYPE, (position_t){
-        .x = rand() % serv->map->height,
-        .y = rand() % serv->map->width
-    });
-    if (!player_entity)
-        return false;
-    if (!(player_data = create_player(arg)))
-        return false;
-    entity_set_data(*player_entity, player_data);
-    return true;
-}
-
 bool command_join(char *arg, player_list_t *player, server_data_t *serv)
 {
     team_t *tmp = NULL;
-    entity_t *player_entity = NULL;
 
     if (!player->is_auth)
         return print_retcode(401, NULL, player->player_peer, false);
@@ -79,9 +59,7 @@ bool command_join(char *arg, player_list_t *player, server_data_t *serv)
     if (tmp->current_members >= tmp->max_members)
         return print_retcode(313, arg, player->player_peer, false);
     tmp->current_members++;
-    if (!command_join_create_player_data(arg, &player_entity, serv))
-        return print_retcode(500, arg, player->player_peer, false);
-    player->player_data = player_entity;
-    entity_wrapper_add_player(serv->entities, player_entity);
+    /// Create a new user and put it in the team
+    /// return print_retcode(301, NULL, player->player_peer, false);
     return print_retcode(211, arg, player->player_peer, true);
 }
