d05970dea6e54ca3c0e6be5296da07610ee76322
Hourcadette Julien <julien.hourcadette@epitech.eu>
2022-06-25 17:45:41 +0200

    [*] return "" instead of None

--- src/zappy_ai_src/ai_function_wrapper.py
+++ src/zappy_ai_src/ai_function_wrapper.py
@@ -143,7 +143,7 @@ class ServerWrapper:
         self.__getRepJoinTeam.restype = ctypes.c_char_p
         c_value = self.__getRepJoinTeam()
         if c_value is None:
-            return None
+            return ""
         return c_value.decode("UTF-8")
 
     def getResponseState(self) -> bool:
